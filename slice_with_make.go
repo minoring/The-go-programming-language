8e70d79e00e1132652ab9eb234f34ddaf816e0ba
minoring <40487883+minoring@users.noreply.github.com>
2018-12-12 00:58:55 +0900

    Update slice_with_make.go

--- slice_with_make.go
+++ slice_with_make.go
@@ -6,17 +6,27 @@ func main() {
 	a := make([]int, 5)
 	printSlice("a", a)
 	
-	b := make([]int, 0, 5)
+	b := make([]int, 5)
 	printSlice("b", b)
 	
-	c := b[:2]
+	c := b[1:3]
+	c[1] = 3
 	printSlice("c", c)
 	
-	d := c[2:5]
+	d := c[1:]
 	printSlice("d", d)
+	
+	d[0] = 1
+	printSlice("b", b)
 }
 
 func printSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n",
 		s, len(x), cap(x), x)
 }
+
+// a len=5 cap=5 [0 0 0 0 0]
+// b len=5 cap=5 [0 0 0 0 0]
+// c len=2 cap=4 [0 3]
+// d len=1 cap=3 [3]
+// b len=5 cap=5 [0 0 1 0 0]
